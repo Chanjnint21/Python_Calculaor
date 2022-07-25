bc95df931708abb117ff7cfbe2b52135f884e917
Chanjnint21 <limchanjnint@gmail.com>
2022-07-25 15:24:54 +0700

    Updated

--- Calculator.py
+++ Calculator.py
@@ -4,19 +4,26 @@ print ("Operation option:\n1. Add (+)\n2. Subtract (-)\n3. multiplication (x)\n4
 user_inp = int(input("\nEnter the operation you want to do as number : "))
 
 def Add():
-    # add = 0
     list = []
     inp = int(input("How many element you want to sum ? :"))
     for i in range (inp):
         num = int(input("Number to add:"))
         list.append(num)
-    sum_add = sum(list)
-    print ("\nSum of umber", list, " is ", sum_add)
+    Add = sum(list)
+    print ("\nSum of umber", list, " is ", Add)
 
 
 
 def Subtract():
-    print ("-")
+    list = []
+    inp = int(input("How many element you want to sabtract ? :"))
+    print("\n***Plz enter the value - in front of the the negative value")
+    for i in range (inp):
+        num = int(input("Number to subtract:"))
+        list.append(num)
+    sub = sum(list)
+    print ("\nSum of umber", list, " is ", sub)
+
 
 def Multiply():
     print ("x")
